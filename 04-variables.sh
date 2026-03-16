97a98e812110c38b9055fb137e0793ec77c99791
SriHari Avula <its.srihari.avula@gmail.com>
2026-03-16 17:23:35 +0530

    variales examples

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hi $PERSON2, How are you ?"
+echo "$PERSON2 :: Hi $PERSON1, I am good. How are You?"
+echo "$PERSON1 :: what are you learning recently?"
+echo "$PERSON2 :: I am learning shell scripting $PERSON1"
