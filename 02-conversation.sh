f27da395feebecf60c216b45e12972122279a3ea
SriHari Avula <its.srihari.avula@gmail.com>
2026-03-16 15:58:16 +0530

    conversation between santhosh and srihari

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Santhosh :: Hi Srihari, How are you ?"
+echo "Srihari :: Hi Santhosh, I am good. How are You?"
+echo "Santhosh :: what are you learning recently?"
+echo "Srihari :: I am learning shell scripting Santhosh"
