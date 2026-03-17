7268debb6383bdb86b3a86f51bb714fff0964b51
SriHari Avula <its.srihari.avula@gmail.com>
2026-03-17 11:47:53 +0530

    prime number find out

---
+++ 07-prime.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo " enter a number: "
+read NUMBER
+flag=0
+for(( i=2;i*i<=NUMBER;i++))
+do
+  if((NUMBER%i==0));then
+  flag=1
+  break
+  fi
+done
+
+if((NUMBER<1));then
+echo "$NUMBER is not prime"
+elif((flag==0));then
+echo "$NUMBER is prime"
+else
+echo "$NUMBER is not prime"
+fi
+
+
+
