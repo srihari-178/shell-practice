851e254627a5a2cb5a069af4b8cf02f999df44a1
SriHari Avula <its.srihari.avula@gmail.com>
2026-03-17 12:15:05 +0530

    id -u to check root access

---
+++ 08-rootaccess.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "check if user has access"
+
+# if [ "$(id -u)" -eq 0 ] ; then ->Test command  
+if (( $(id -u) == 0 )) ; then  # Arithematic style
+echo "he is in root access"
+else
+echo "he is not in root access"
+fi
